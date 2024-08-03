99a79114a16440d243bb1d6bdbd6589cd3bdac93
awais512 <raza.awais25@gmail.com>
2024-08-04 00:03:38 +0100

    Follow and Unfollow functionality

--- app/api/users/[userId]/followers/route.ts
+++ app/api/users/[userId]/followers/route.ts
@@ -53,26 +53,30 @@ export async function POST(
 ) {
   try {
     const { user: loggedInUser } = await validateRequest();
+
     if (!loggedInUser) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
-    await prisma.follow.upsert({
-      where: {
-        followerId_followingId: {
+
+    await prisma.$transaction([
+      prisma.follow.upsert({
+        where: {
+          followerId_followingId: {
+            followerId: loggedInUser.id,
+            followingId: userId,
+          },
+        },
+        create: {
           followerId: loggedInUser.id,
           followingId: userId,
         },
-      },
-      create: {
-        followerId: loggedInUser.id,
-        followingId: userId,
-      },
-      update: {},
-    });
+        update: {},
+      }),
+    ]);
 
     return new Response();
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
   }
 }
